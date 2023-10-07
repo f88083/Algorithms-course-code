76109873f25a6214bbccc4497ceb14471f293728
f88083 <f88083@gmail.com>
2023-10-07 22:44:18 +0100

    Update files and added week 2 materials

---
+++ hello/Deque.java
@@ -0,0 +1,61 @@
+/* *****************************************************************************
+ *  Name:              Shih-Che, Lai
+ *  Coursera User ID:  123456
+ *  Last modified:     2023/10/7
+ **************************************************************************** */
+
+/*
+ * Your submission may not call library functions except those in StdIn, StdOut,
+ * StdRandom, java.lang, java.util.Iterator, and java.util.NoSuchElementException
+ * In particular, do not use either java.util.LinkedList or java.util.ArrayList.
+ */
+
+public class Deque<Item> implements Iterable<Item> {
+    private Item item;
+
+    // construct an empty deque
+    public Deque() {
+
+    }
+
+    // is the deque empty?
+    public boolean isEmpty() {
+        return true;
+    }
+
+    // return the number of items on the deque
+    public int size() {
+        return 0;
+    }
+
+    // add the item to the front
+    public void addFirst(Item item) {
+
+    }
+
+    // add the item to the back
+    public void addLast(Item item) {
+
+    }
+
+    // remove and return the item from the front
+    public Item removeFirst() {
+        return item;
+    }
+
+    // remove and return the item from the back
+    public Item removeLast() {
+        return item;
+    }
+
+    // return an iterator over items in order from front to back
+    // public Iterator<Item> iterator() {
+    //
+    // }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ hello/Watch.java
@@ -0,0 +1,17 @@
+/* *****************************************************************************
+ *  Name:              Shih-Che, Lai
+ *  Coursera User ID:  123456
+ *  Last modified:     2023/10/1
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.Stopwatch;
+
+public class Watch {
+    public static void main(String[] args) {
+        int a = 1, b = 2, c = 0;
+        Stopwatch watch = new Stopwatch();
+        c = a + b;
+        System.out.println(watch.elapsedTime());
+
+    }
+}
