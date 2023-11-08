23440b8ce6eecdae10a02fecf9fdb5110a9a507f
f88083 <f88083@gmail.com>
2023-11-08 16:24:24 +0000

    Cleaned codes, developing BruteCollinearPoints.java

--- collinear/BruteCollinearPoints.java
+++ collinear/BruteCollinearPoints.java
@@ -5,21 +5,33 @@
  **************************************************************************** */
 
 public class BruteCollinearPoints {
+    private int numLineSeg; // Number of line segments
 
     // finds all line segments containing 4 points
     public BruteCollinearPoints(Point[] points) {
+        if (points == null) throw new IllegalArgumentException();
 
+        // Check item null and duplicates
+        for (int i = 0; i < points.length; i++) {
+            if (points[i] == null) throw new IllegalArgumentException();
+            for (int j = i + 1; j < points.length; j++) {
+                if (points[j] == null) throw new IllegalArgumentException();
+                // Check duplicates
+                if (points[i].compareTo(points[j]) == 0) throw new IllegalArgumentException();
+            }
+        }
+
+        numLineSeg = 0;
     }
 
     // the number of line segments
     public int numberOfSegments() {
-        // FIXME
-        return 0;
+        return numLineSeg;
     }
 
     // the line segments
     public LineSegment[] segments() {
-        // FIXME
+        // TODO
         return new LineSegment[1];
     }
 
--- collinear/Point.java
+++ collinear/Point.java
@@ -60,14 +60,12 @@ public class Point implements Comparable<Point> {
      * @return the slope between this point and the specified point
      */
     public double slopeTo(Point that) {
-        /* YOUR CODE HERE */
-
         // Vertical
         if (this.x == that.x && this.y != that.y) return Double.POSITIVE_INFINITY;
-            // Horizontal
-        else if (this.y == that.y && this.x != that.x) return 0.0;
-            // Same postion
-        else if (this.x == that.x && this.y == that.y) return Double.NEGATIVE_INFINITY;
+        // Horizontal
+        if (this.y == that.y && this.x != that.x) return 0.0;
+        // Same postion
+        if (this.x == that.x && this.y == that.y) return Double.NEGATIVE_INFINITY;
 
         return (that.y - this.y) / (that.x - this.x);
     }
@@ -85,12 +83,10 @@ public class Point implements Comparable<Point> {
      * argument point
      */
     public int compareTo(Point that) {
-        /* YOUR CODE HERE */
         if (this.y < that.y) return -1;
         if (this.y == that.y && this.x < that.x) return -1;
         // Equal
         if (this.x == that.x && this.y == that.y) return 0;
-        // FIXME
         return 1;
     }
 
@@ -101,8 +97,6 @@ public class Point implements Comparable<Point> {
      * @return the Comparator that defines this ordering on points
      */
     public Comparator<Point> slopeOrder() {
-        // FIXME
-        /* YOUR CODE HERE */
         return new Comparator<Point>() {
             public int compare(Point p1, Point p2) {
                 return p1.compareTo(p2);
